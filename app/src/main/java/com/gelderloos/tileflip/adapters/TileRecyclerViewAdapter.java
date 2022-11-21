41593cda26d0c429c47e57fd2573a9e335343781
Lucy Gelderloos <lucygel@gmail.com>
2022-11-21 15:52:44 -0800

    All tiles generate with on-click listener for flip animation

--- app/src/main/java/com/gelderloos/tileflip/adapters/TileRecyclerViewAdapter.java
+++ app/src/main/java/com/gelderloos/tileflip/adapters/TileRecyclerViewAdapter.java
@@ -1,5 +1,7 @@
 package com.gelderloos.tileflip.adapters;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -34,6 +36,29 @@ public class TileRecyclerViewAdapter extends RecyclerView.Adapter<TileRecyclerVi
     public void onBindViewHolder(@NonNull TileViewHolder holder, int position) {
         ImageView tileBackImageView = holder.itemView.findViewById(R.id.imgViewTileBack);
         tileBackImageView.setImageResource(R.drawable.tileback);
+        ImageView tileFrontImageView = holder.itemView.findViewById(R.id.imgViewTileFront);
+        tileFrontImageView.setImageResource(R.drawable.tile1);
+        tileBackImageView.setOnClickListener(view -> {
+            flipTile(this.callingActivity,tileBackImageView,tileFrontImageView);
+        });
+    }
+
+    private void flipTile(Context context, View visibleView, View invisibleView) {
+//        https://medium.com/geekculture/how-to-add-card-flip-animation-in-the-android-app-3060afeadd45
+        visibleView.setVisibility(View.VISIBLE);
+        float scale = context.getResources().getDisplayMetrics().density;
+        float cameraDist = 8000 * scale;
+        invisibleView.setCameraDistance(cameraDist);
+        visibleView.setCameraDistance(cameraDist);
+        AnimatorSet flipOutAnimatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(context, R.animator.back_animator);
+        flipOutAnimatorSet.setTarget(invisibleView);
+        AnimatorSet flipInAnimatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(context, R.animator.front_animator);
+        flipInAnimatorSet.setTarget(visibleView);
+        flipOutAnimatorSet.start();
+        flipInAnimatorSet.start();
+//        invisibleView.setVisibility(View.GONE);
+//        need java version of flipInAnimatorSet.doOnEnd { invisibleView.gone() }
+
     }
 
     @Override
--- app/src/main/java/com/gelderloos/tileflip/fragments/TileFragment.java
+++ app/src/main/java/com/gelderloos/tileflip/fragments/TileFragment.java
@@ -11,13 +11,17 @@ import android.view.ViewGroup;
 import com.gelderloos.tileflip.R;
 
 public class TileFragment extends Fragment {
+    private String tileId;
+    private String tileValue;
 
     public TileFragment() {
         // Required empty public constructor
     }
 
-    public static TileFragment newInstance(String param1, String param2) {
+    public static TileFragment newInstance(String tileId, String tileValue) {
         TileFragment fragment = new TileFragment();
+        fragment.setTileId(tileId);
+        fragment.setTileValue(tileValue);
         return fragment;
     }
 
@@ -31,5 +35,21 @@ public class TileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tile, container, false);
+    }
+
+    public String getTileId() {
+        return tileId;
+    }
+
+    public void setTileId(String tileId) {
+        this.tileId = tileId;
+    }
+
+    public String getTileValue() {
+        return tileValue;
+    }
+
+    public void setTileValue(String tileValue) {
+        this.tileValue = tileValue;
     }
 }
