af844b832f7176ff001e00c16e7f68aa417acebe
Lucy Gelderloos <lucygel@gmail.com>
2022-12-08 21:16:25 -0800

    Cleanup

--- app/src/main/java/com/gelderloos/tileflip/fragments/WinGameDialogFragment.java
+++ app/src/main/java/com/gelderloos/tileflip/fragments/WinGameDialogFragment.java
@@ -9,20 +9,14 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.Fragment;
 
 import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.gelderloos.tileflip.MainActivity;
 import com.gelderloos.tileflip.R;
-
-import org.w3c.dom.Text;
-
-import java.util.Objects;
 
 public class WinGameDialogFragment extends DialogFragment {
     private int score;
